2b58051de7d02f5479b6e3c496ead55564639d96
Fangzhen Song <songfangzhen@bytedance.com>
2021-08-31 21:16:32 +0800

    Fix Gerrit
    This change fixes the issue that cannot be opened from left diff
    view on Gerrit.

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ class ChromiumHandler {
 	// https://source.chromium.org/chromium/chromium/src/+/main:base/values.h;l=104
 	constructor(url) {
 		this.url = url;
-		url = url.replace("https://source.chromium.org/chromium","");
+		url = url.replace('https://source.chromium.org/chromium', '');
 		url = url.split(':')[1];
 		if (!url) return;
 
@@ -11,9 +11,9 @@ class ChromiumHandler {
 		if (!file) return;
 
 		// Get the line number
-		let line = url.split(';l=')[1].split(';')[0].split("?")[0];
+		let line = url.split(';l=')[1].split(';')[0].split('?')[0];
 		if (!line)
-			line = "1";
+			line = '1';
 
 		this.file = file;
 		this.line = line;
@@ -25,7 +25,7 @@ class GerritHandler {
 	// https://chromium-review.googlesource.com/c/chromium/src/+/3086892/10/chrome/browser/sync/test/integration/two_client_web_apps_bmo_sync_test.cc#646
 	constructor(url) {
 		this.url = url;
-		url = url.replace("https://chromium-review.googlesource.com/c/chromium/src/+/","");
+		url = url.replace('https://chromium-review.googlesource.com/c/chromium/src/+/', '');
 
 		// Get the file path
 		const chunk = url.split('#')[0];
@@ -34,24 +34,27 @@ class GerritHandler {
 		let indexOfFirst = chunk.indexOf(SEARCH_TERM);
 		const NOT_FIND = -1;
 		if (indexOfFirst == NOT_FIND) {
-			console.log("First index of" + SEARCH_TERM + " not find");
+			console.log('First index of' + SEARCH_TERM + ' not find!');
 			return;
 		}
 		let indexOfSecond = chunk.indexOf(SEARCH_TERM, indexOfFirst + 1);
 		if (indexOfSecond == NOT_FIND) {
-			console.log("Second index of" + SEARCH_TERM + " not find");
+			console.log('Second index of' + SEARCH_TERM + ' not find!');
 			return;
 		}
 		
 		let file = chunk.substr(indexOfSecond + 1);
 		if (!file) return;
 
-		// Get the line number
+		// Get the line number.
 		let line = url.split('#')[1];
 		if (line) {
-			line.replace('d', '');
+			// If chose from left diff view on Gerrit, there will be a 'b' in 
+			// the line number which should get removed.
+			line = line.replace('b', '');
 		} else {
-			line = "1";
+			// Default to open and locate at the first line.
+			line = '1';
 		}
 
 		this.file = file;
@@ -81,16 +84,16 @@ function RequestOpen(url) {
 	let handler = new handlerClass(url);
 
 	if (!handler || !handler.success) return;
-	console.log("send", handler);
+	console.log('send', handler);
 	let param = Json2Get(handler);
 	console.log(param);
-	let reqUrl = "http://127.0.0.1:8989/file?" + param;
+	let reqUrl = 'http://127.0.0.1:8989/file?' + param;
 	fetch(reqUrl)
 	.then((response) => {
-		console.log("Response: ", response);
+		console.log('Response: ', response);
 	})
 	.catch((error) => {
-		console.log("Error: ", error);
+		console.log('Error: ', error);
 	});
 };
 
